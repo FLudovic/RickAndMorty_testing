e0f9741709e89fd8e9485ea9e3da5e6b769d2861
FLudovic <ludovic.flament.99@gmail.com>
2022-04-14 01:54:15 -0700

    test ci

--- Front/src/test/hooks/useProduct.test.tsx
+++ Front/src/test/hooks/useProduct.test.tsx
@@ -1,38 +1,35 @@
-import {rest} from "msw";
-import {setupServer} from "msw/node";
-import { renderHook, act } from '@testing-library/react-hooks'
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { renderHook, act } from "@testing-library/react-hooks";
 import useProduct from "../../hooks/useProduct";
 
-//jest.setTimeout(80000);
-
 const server = setupServer(
-    rest.get(
-        "http://localhost:8000/api/cart/5",
-        (req, res, ctx) => {
-            return res(
-                ctx.json({"error":"too many"})
-        )}
-    ),
-    // remove
+  rest.post("http://localhost:8000/api/cart/2", (req, res, ctx) => {
+    return res(
+      ctx.json({"error":"too many"})
+    );
+  })
 );
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-    test("load product", async () => {
-        const product = {
-            "id":5,
-            "name":"Jerry Smith",
-            "price":"8",
-            "quantity":30,
-            "image":"https://rickandmortyapi.com/api/character/avatar/5.jpeg"
-        }
-        const { result } = renderHook(() => useProduct(product));
-        const { loading, addProduct } = result.current;
-        expect(loading).toEqual(false);
-        await act(async () => {
-            await addProduct();
-        }); 
-        const { message } = result.current;
-    });
+test("Load product", async () => {
+  const { result } = renderHook(() =>
+    useProduct({
+      id: 2,
+      name: "Rick Sanchez",
+      price: "9,99",
+      quantity: 0,
+      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    })
+  );
+  const { loading, addProduct } = result.current;
+  expect(loading).toEqual(false);
+  await act(async () => {
+    await addProduct();
+  }); 
+  const { message } = result.current;
+//   console.log(message);
+});
